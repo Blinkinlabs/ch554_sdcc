be6af9b35c7df2021c09f9bd590d8b329eabe1fa
Nerd Ralph <ralphdoncaster@gmail.com>
2021-02-23 21:04:55 -0400

    simplified HID report descriptor

--- examples/CMSIS_DAP/USBconstant.c
+++ examples/CMSIS_DAP/USBconstant.c
@@ -74,16 +74,10 @@ __code uint8_t ReportDesc[] ={
     0x06, 0x00, 0xFF,   // Usage Page = 0xFF00 (Vendor Defined Page 1)
     0x09, 0x01,         // Usage (Vendor Usage 1)
     0xA1, 0x01,         // Collection (Application)
-    0x19, 0x01,         // Usage Minimum
-    0x29, 0x40,         // Usage Maximum //64 input usages total (0x01 to 0x40)
-    0x15, 0x00,         // Logical Minimum (data bytes in the report may have minimum value = 0x00)
-    0x26, 0xFF, 0x00,   // Logical Maximum (data bytes in the report may have maximum value = 0x00FF = unsigned 255)
     0x75, 0x08,         // Report Size: 8-bit field size
-    0x95, 0x40,         // Report Count: Make sixty-four 8-bit fields (the next time the parser hits an "Input", "Output", or "Feature" item)
-    0x81, 0x00,         // Input (Data, Array, Abs): Instantiates input packet fields based on the above report size, count, logical min/max, and usage.
-    0x19, 0x01,         // Usage Minimum
-    0x29, 0x40,         // Usage Maximum //64 output usages total (0x01 to 0x40)
-    0x91, 0x00,         // Output (Data, Array, Abs): Instantiates output packet fields. Uses same report size and count as "Input" fields, since nothing new/different was specified to the parser since the "Input" item.
+    0x95, 0x40,         // Report Count: Make sixty-four fields
+    0x81, 0x01,         // Input (Const,Array,Abs,No Wrap,Linear)
+    0x91, 0x01,         // Output (Const,Array,Abs,No Wrap,Linear)
     0xC0                // End Collection
 };
 __code uint8_t ReportDescLen = sizeof(ReportDesc);
